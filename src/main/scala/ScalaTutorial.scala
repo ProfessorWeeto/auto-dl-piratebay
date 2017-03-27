7e1b5a931400c87f991f8aea26e907499463c3ef
Michael.Davies <reflexkumi@gmail.com>
2017-03-27 16:34:52 +0100

    Anagram actually works now with a simple Shuffle o' The Buffer

--- src/main/scala/ScalaTutorial.scala
+++ src/main/scala/ScalaTutorial.scala
@@ -1,31 +1,17 @@
+import scala.util.Random._
+
 object ScalaTutorial {
 
-  def main(args:Array[String]): Unit = {
-    var i = 0   //index for the loop
-    val letters = "Anagram"
-    val arrayLength = letters.length
-    val wordArray = new Array[String](arrayLength + 1)
-    val word = new StringBuilder(arrayLength + 1)
+  def main(): Unit = {
+    val input = "Michael"
 
-    do {
-      // for each loop round, generate a new random number between 0 and the length of the word, then round it to the nearest integer
-
-      val randomNumber = (Math.random() * arrayLength).toInt
-      // for each loop round, grab the letter at the index of i (in the string) and put it into the array at the same index
-      wordArray(i) = letters(randomNumber).toString
-      // using the StringBuilder object, append the letter stored in the array at index i to the word string
-      word.append(wordArray(i))
-      // TODO: remove an item from the array
-      removeItem(wordArray, i)
-      i+=1 // increment the index on the loop
-    } while (i < arrayLength)
+    makeAnagram(input)
 
   }
+  def makeAnagram(string: String): Unit = {
+    val wordList  = string.toBuffer
+    val shuffledList = shuffle(wordList)
 
-  def removeItem(array: Array[String], index: Int): Array[String] = {
-    val buffer = array.toBuffer
-    buffer.remove(index)
-    println(buffer)
-    buffer.toArray
+    println(shuffledList.mkString(""))
   }
 }
